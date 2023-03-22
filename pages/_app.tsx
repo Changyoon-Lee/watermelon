0c4f60e014c6e426b87003ea0b6caaff30e01ec2
Changyoon-Lee <lkingkongl@naver.com>
2023-03-22 19:06:29 +0900

    init

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+// import { SWRConfig } from "swr";
+import "@styles/globals.css";
+
+export default function App({ Component, pageProps }: any) {
+  return (
+    // <SWRConfig
+    //   value={{
+    //     fetcher: (url: string) => fetch(url).then((response) => response.json())
+    //   }}
+    // >
+      <div className="w-full max-w-xl m-auto">
+        <Component {...pageProps} />
+      </div>
+    // {/* </SWRConfig> */}
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+function HomePage() {
+    return <div className="text-red-500 font-bold text-4xl">Welcome to Next.js!</div>
+  }
+  
+  export default HomePage
